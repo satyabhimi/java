75a5d2bc4a360ecf6d0163d74dc3c244cf9b4e04
satya <satya.bhimisetty@gmail.com>
2021-06-20 13:01:40 +0530

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 20;
+
+        long b = (long) a;
+
+        System.out.println(b);
+
+        long x = 200;
+
+        float y = x;
+
+        System.out.println(y);
+    }
+}
